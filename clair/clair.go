d753db227996c0739ff45d600ad1c1fdf3001653
David Xia <dxia@spotify.com>
2017-04-21 17:51:03 -0400

    Fix report command
    The report command currently analyzes the oldest layer.
    This is incorrect. It should analyze the most recent layer.
    See the description for clair's API [here](https://github.com/coreos/clair/blob/master/Documentation/api_v1.md#get-layersname).

--- clair/clair.go
+++ clair/clair.go
@@ -27,7 +27,7 @@ func (imageAnalysis ImageAnalysis) String() string {
 
 //LastLayer return the last layer of ImageAnalysis
 func (imageAnalysis ImageAnalysis) LastLayer() *v1.Layer {
-	return imageAnalysis.Layers[len(imageAnalysis.Layers)-1].Layer
+	return imageAnalysis.Layers[0].Layer
 }
 
 func (imageAnalysis ImageAnalysis) CountVulnerabilities(l v1.Layer) int {
